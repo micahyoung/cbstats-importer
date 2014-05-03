5d36cf73c0a01f881f77a46791d213be75b8f9af
Micah Young <micah@young.io>
2014-05-02 23:12:15 -0400

    Remove additional business logic from parallel importer

--- lib/cbstats_importer/import.ex
+++ lib/cbstats_importer/import.ex
@@ -10,15 +10,20 @@ defmodule Mix.Tasks.Import do
   def run(args) do
     Mix.Task.run "app.start", []
 
-    options = elem(OptionParser.parse(args), 0)
+    options = elem(OptionParser.parse(args, switches: [processes: :integer]), 0)
     data_root = options[:path] || "data/json"
 
     files = Path.wildcard(Path.join(data_root, "*"))
+    child_fun = fn(file) ->
+      CbstatsImporter.ImportProcess.import_path(file)
+      {:ok, nil}
+    end
     callback_fun = fn(_file, last_counter) ->
       update_counter = last_counter || CbstatsImporter.RateCount.init(length(files))
-      CbstatsImporter.RateCount.update(update_counter)
+      new_counter = CbstatsImporter.RateCount.update(update_counter)
+      {:ok, new_counter}
     end
-    CbstatsImporter.ParallelImporter.import(files, callback_fun)
+    CbstatsImporter.ParallelImporter.import(files, child_fun, callback_fun)
   end
 end
 
--- lib/cbstats_importer/parallel_importer.ex
+++ lib/cbstats_importer/parallel_importer.ex
@@ -1,56 +1,56 @@
 defmodule CbstatsImporter.ParallelImporter do
-  defrecord Callback, fun: nil, last_result: nil
+  defrecord Record, child_fun: nil, callback_fun: nil, callback_acc: nil
 
-  def import(files, callback_fun) do
-    callback = Callback.new fun: callback_fun
-    spawn_importers(files, [], callback)
+  def import(files, child_fun, callback_fun) do
+    record = Record.new child_fun: child_fun, callback_fun: callback_fun
+    spawn_importers(files, [], record)
   end
 
   # We already have 4 currently running, don't spawn new ones
-  defp spawn_importers(files, queued, callback) when
+  defp spawn_importers(files, queued, record) when
       length(queued) >= 4 do
     # IO.puts "We already have 4 currently running, don't spawn new ones"
-    wait_for_messages(files, queued, callback)
+    wait_for_messages(files, queued, record)
   end
 
   # Spawn a compiler for each file in the list until we reach the limit
-  defp spawn_importers([h|t], queued, callback) do
+  defp spawn_importers([h|t], queued, record) do
     # IO.puts "Spawn a compiler for each file in the list until we reach the limit"
     parent = Process.self()
 
     child = spawn_link fn ->
       try do
-        {:ok, 1} = CbstatsImporter.ImportProcess.import_path(h)
-        send parent, { :imported, Process.self(), h }
+        {:ok, result} = record.child_fun.(h)
+        send parent, { :imported, Process.self(), result }
       catch
         kind, reason ->
           send parent, { :failure, Process.self(), kind, reason, System.stacktrace }
       end
     end
 
-    spawn_importers(t, [{child,h}|queued], callback)
+    spawn_importers(t, [{child,h}|queued], record)
   end
 
   # No more files, nothing waiting, queue is empty, we are done
-  defp spawn_importers([], [], _callback) do
+  defp spawn_importers([], [], _record) do
     # IO.puts "No more files, nothing waiting, queue is empty, we are done"
     :done
   end
 
   # No more files, but queue and waiting are not full or do not match
-  defp spawn_importers([], queued, callback) do
+  defp spawn_importers([], queued, record) do
     # IO.puts "No more files, but queue and waiting are not full or do not match"
-    wait_for_messages([], queued, callback)
+    wait_for_messages([], queued, record)
   end
 
   # Wait for messages from child processes
-  defp wait_for_messages(files, queued, callback) do
+  defp wait_for_messages(files, queued, record) do
     receive do
-      { :imported, child, file } ->
+      { :imported, child, result } ->
         new_queued  = List.keydelete(queued, child, 0)
-        result = callback.fun.(file, callback.last_result)
-        new_callback = callback.update(last_result: result)
-        spawn_importers(files, new_queued, new_callback)
+        {:ok, new_acc} = record.callback_fun.(result, record.callback_acc)
+        new_record = record.update(callback_acc: new_acc)
+        spawn_importers(files, new_queued, new_record)
       { :failure, child, kind, reason, stacktrace } ->
         IO.puts "child: failure"
 
